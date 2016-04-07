25d4ee26088e196850332b79971cfd4247c3847e
jianhong <jianhong.yang@chukong-inc.com>
2016-04-07 17:47:18 +0800

    Demo中使用UIImageView+NBL
    Demo中使用UIImageView+NBL

--- SimplifiedAFN/SecondViewController.m
+++ SimplifiedAFN/SecondViewController.m
@@ -8,9 +8,10 @@
 
 #import "SecondViewController.h"
 #import "NBLHTTPFileManager.h"
+#import "UIImageView+NBL.h"
 
 @interface SecondViewController ()
-
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation SecondViewController
@@ -19,18 +20,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSString *urlFile = @"http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.0.6.dmg";
-//    urlFile = @"http://cc.cocimg.com/api/uploads/20151113/1447401023180293.png";
-    [[NBLHTTPFileManager sharedManager] downloadFile:nil from:urlFile withParam:@{} progress:^(int64_t bytesReceived, int64_t totalBytes, NSDictionary *dicParam) {
-        NSLog(@"%lld (%lld)(%.2f%%)", bytesReceived, totalBytes, 100.0f*bytesReceived/totalBytes);
-    } andResult:^(NSString *filePath, NSHTTPURLResponse *httpResponse, NSError *error, NSDictionary *dicParam) {
-        if (nil == error) {
-            NSLog(@"保存路径：%@", filePath);
-        }
-        else {
-            NSLog(@"下载失败：%@", error);
-        }
-        NSLog(@"回传参数：%@", dicParam);
+//    NSString *urlFile = @"http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.0.6.dmg";
+//    urlFile = @"http://cocostudio.download.appget.cn/CocosCreator/v1.0.0/CocosCreator_v1.0.0_2016032904.dmg";
+//    [[NBLHTTPFileManager sharedManager] downloadFile:nil from:urlFile withParam:@{} progress:^(int64_t bytesReceived, int64_t totalBytes, NSDictionary *dicParam) {
+//        NSLog(@"%lld (%lld)(%.2f%%)", bytesReceived, totalBytes, 100.0f*bytesReceived/totalBytes);
+//    } andResult:^(NSString *filePath, NSHTTPURLResponse *httpResponse, NSError *error, NSDictionary *dicParam) {
+//        if (nil == error) {
+//            NSLog(@"保存路径：%@", filePath);
+//        }
+//        else {
+//            NSLog(@"下载失败：%@", error);
+//        }
+//        NSLog(@"回传参数：%@", dicParam);
+//    }];
+    
+    NSString *urlPic = @"http://www.bjsubway.com/subway/images/subway_map.jpg";
+    [self.imageView loadImageFromCachePath:nil orPicUrl:urlPic withDownloadResult:^(UIImageView *imageView, NSString *picUrl, float progress, BOOL finished, NSError *error) {
+        
     }];
 }
 
@@ -41,6 +47,7 @@
 
 - (void)dealloc
 {
+    [self.imageView cancelDownload];
     NSLog(@"释放 %s", object_getClassName(self));
 }
 
